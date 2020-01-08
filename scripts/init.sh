832b6de0872a2ecb445a7f58531a94a2eecd4cf3
Zikun Fan <zikunvan@gmail.com>
2020-01-08 16:40:57 +0800

    init with aura/grandpa consensus

---
+++ scripts/init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "*** Initializing WASM build environment"
+
+if [ -z $CI_PROJECT_NAME ] ; then
+   rustup update nightly
+   rustup update stable
+fi
+
+# do not need wasm for the alpha version
+# rustup target add wasm32-unknown-unknown --toolchain nightly

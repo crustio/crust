9a1e1b25d19e7f80bf24c0945fe786b1a9cd6bde
Zikun Fan <zikunvan@gmail.com>
2020-03-26 19:06:53 +0800

    fix test case

--- cstrml/staking/src/lib.rs
+++ cstrml/staking/src/lib.rs
@@ -1131,7 +1131,7 @@ impl<T: Trait> Module<T> {
     fn upsert_guarantee(v_stash: &T::AccountId,
                         g_stash: &T::AccountId,
                         g_votes: BalanceOf<T>) -> bool {
-        if !<Validators<T>>::exists(v_stash) || <Validators<T>>::exists(g_stash) {
+        if !<Validators<T>>::exists(v_stash) || g_stash == v_stash {
             // v_stash is not validator or you want to vote your self
             // you vote NOTHING 🙂
             return false;

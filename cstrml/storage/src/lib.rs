49a1ac39141f7134063b262d7b9b562a3218a82b
myron <qiufan_glamour@hotmail.com>
2020-03-30 22:48:50 +0800

    init and draft version for storage/lib.rs

---
+++ cstrml/storage/src/lib.rs
@@ -0,0 +1,92 @@
+//! The Substrate Node runtime. This can be compiled with `#[no_std]`, ready for Wasm.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![feature(option_result_contains)]
+
+use codec::{Decode, Encode};
+use frame_support::{decl_event, decl_module, decl_storage, dispatch::DispatchResult, ensure};
+use sp_std::convert::TryInto;
+use sp_std::{str, vec::Vec};
+use system::ensure_signed;
+
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+
+// Crust runtime modules
+use primitives::{constants::tee::*, MerkleRoot, PubKey, TeeSignature};
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[derive(Debug, PartialEq, Eq, Clone, Encode, Decode, Default)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct StorageOrder<T> { // Need to confirm this name. FileOrder?
+    pub file_indetify: MerkleRoot,
+    pub file_size: u64,
+    pub expired_duration: u64,
+    pub expired_on: u64
+}
+
+/// The module's configuration trait.
+pub trait Trait: system::Trait {
+    /// The overarching event type.
+    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+    /// To do. Support work report check
+}
+
+// TODO: add add_extra_genesis to unify chain_spec
+// This module's storage items.
+decl_storage! {
+    trait Store for Module<T: Trait> as Tee {
+        pub StorageOrders get(fn storage_orders) config(): map T::AccountId => Option<StorageOrder>; //这个结构要再考虑一下 List of Option?
+    }
+}
+
+// The module's dispatchable functions.
+decl_module! {
+    /// The module declaration.
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        // Initializing events
+        // this is needed only if you are using events in your module
+        fn deposit_event() = default;
+
+        fn store_storage_order(origin, storage_order: StorageOrder) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            // 1. Do check and should do something
+            ensure!(Self::storage_order_check(&storage_order).is_ok(), "Storage Order is invalid!");
+
+            // 2. Store the storage order
+            <StorageOrders<T>>::insert(&who, &StorageOrder);
+
+            // 3. Emit storage order event
+            Self::deposit_event(RawEvent::ReportWorks(who, work_report));
+
+            Ok(())
+        }
+    }
+}
+
+impl<T: Trait> Module<T> {
+    // IMMUTABLE PUBLIC
+    pub fn storage_order_check_pub(so: &StorageOrder) -> DispatchResult {
+        Ok(())
+    }
+
+    // private function can be built in here
+    fn storage_order_check(so: &StorageOrder) -> DispatchResult {
+        Ok(())
+    }
+}
+
+decl_event!(
+    pub enum Event<T>
+    where
+        AccountId = <T as system::Trait>::AccountId,
+    {
+        StorageOrders(AccountId, StorageOrder),
+    }
+);

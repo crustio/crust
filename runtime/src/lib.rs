2750de10468d03da17e353860cbc650b5e4ae77d
Zikun Fan <zikunvan@gmail.com>
2020-05-22 18:44:10 +0800

    HOTFIX checks wr by order id

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -335,7 +335,7 @@ impl transaction_payment::Trait for Runtime {
 impl tee::Trait for Runtime {
     type Event = Event;
     type Works = Staking;
-    type MarketInterface = ();
+    type MarketInterface = Self;
 }
 
 impl market::Trait for Runtime {

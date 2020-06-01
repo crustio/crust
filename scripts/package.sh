949c4df7f7cd29f73bfb7b95af304d4e6852bdf0
Zikun Fan <zikunvan@gmail.com>
2020-06-01 15:36:50 +0800

    add cargo build in package shell

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,24 +3,26 @@ version="0.3.0"
 
 # check if is built
 if [ ! -f target/release/crust ]; then
-    echo "No executable file, you should run `cargo build --release` first."
-else
-    if [ -d crust/ ]; then rm -Rf crust/; fi
-    
-    # create crust folder
-    mkdir crust
-    mkdir crust/bin
-    
-    # create and fillin VERSION file
-    touch crust/VERSION
-    echo $version >> crust/VERSION
-
-    # copy crust
-    cp target/release/crust crust/bin/
-
-    # package
-    tar -cvf crust.tar crust
-
-    # delete crust folder
-    rm -Rf crust/
+    echo 'No executable crust file, building now...'
+    cargo build --release
 fi
+
+if [ -d crust/ ]; then rm -Rf crust/; fi
+
+# create crust folder
+mkdir crust
+mkdir crust/bin
+
+# create and fillin VERSION file
+touch crust/VERSION
+echo $version >> crust/VERSION
+
+# copy crust
+cp target/release/crust crust/bin/
+
+# package
+tar -cvf crust.tar crust
+
+# delete crust folder
+rm -Rf crust/
+

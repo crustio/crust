287d63c9671d287130d9dcce209860522bae259c
myron <qiufan_glamour@hotmail.com>
2020-07-14 20:26:17 +0800

    add benchmarks

---
+++ node/src/executor.rs
@@ -0,0 +1,11 @@
+pub use sc_executor::NativeExecutor;
+use sc_executor::native_executor_instance;
+
+// Declare an instance of the native executor named `Executor`. Include the wasm binary as the
+// equivalent wasm code.
+native_executor_instance!(
+	pub Executor,
+    crust_runtime::api::dispatch,
+    crust_runtime::native_version,
+    (frame_benchmarking::benchmarking::HostFunctions, cstrml_tee::api::crypto::HostFunctions),
+);

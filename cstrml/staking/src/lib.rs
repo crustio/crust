14296739d70f60712f5937474209199066838cef
myron <qiufan_glamour@hotmail.com>
2020-09-21 21:43:28 +0800

    rename

--- cstrml/staking/src/lib.rs
+++ cstrml/staking/src/lib.rs
@@ -64,7 +64,7 @@ pub type RewardPoint = u32;
 
 /// Reward points of an era. Used to split era total payout between validators.
 ///
-/// This points will be used to reward validators and their respective nominators.
+/// This points will be used to reward validators and their respective guarantors.
 #[derive(PartialEq, Encode, Decode, Default, RuntimeDebug)]
 pub struct EraRewardPoints<AccountId: Ord> {
     /// Total number of points. Equals the sum of reward points for each validator.
@@ -106,7 +106,7 @@ impl Default for RewardDestination {
 #[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
 pub struct ValidatorPrefs {
     /// Reward that validator takes up-front; only the rest is split between themselves and
-    /// nominators.
+    /// guarantors.
     #[codec(compact)]
     pub fee: Perbill,
 }
@@ -389,10 +389,10 @@ pub trait Trait: frame_system::Trait {
     /// Number of eras that staked funds must remain bonded for.
     type BondingDuration: Get<EraIndex>;
 
-    /// The maximum number of nominators rewarded for each validator.
+    /// The maximum number of guarantors rewarded for each validator.
     ///
     /// For each validator only the `$MaxGuarantorRewardedPerValidator` biggest stakers can claim
-    /// their reward. This used to limit the i/o cost for the nominator payout.
+    /// their reward. This used to limit the i/o cost for the guarantor payout.
     type MaxGuarantorRewardedPerValidator: Get<u32>;
 
     /// Number of eras that slashes are deferred by, after computation. This
@@ -476,10 +476,10 @@ decl_storage! {
 
         /// Clipped Exposure of validator at era.
         ///
-        /// This is similar to [`ErasStakers`] but number of nominators exposed is reduced to the
+        /// This is similar to [`ErasStakers`] but number of guarantors exposed is reduced to the
         /// `T::MaxGuarantorRewardedPerValidator` biggest stakers.
         /// (Note: the field `total` and `own` of the exposure remains unchanged).
-        /// This is used to limit the i/o cost for the nominator payout.
+        /// This is used to limit the i/o cost for the guarantor payout.
         ///
         /// This is keyed fist by the era index to allow bulk deletion and then the stash account.
         ///
@@ -701,10 +701,10 @@ decl_module! {
         /// Number of eras that staked funds must remain bonded for.
         const BondingDuration: EraIndex = T::BondingDuration::get();
 
-        /// The maximum number of nominators rewarded for each validator.
+        /// The maximum number of guarantors rewarded for each validator.
         ///
         /// For each validator only the `$MaxGuarantorRewardedPerValidator` biggest stakers can claim
-        /// their reward. This used to limit the i/o cost for the nominator payout.
+        /// their reward. This used to limit the i/o cost for the guarantor payout.
         const MaxGuarantorRewardedPerValidator: u32 = T::MaxGuarantorRewardedPerValidator::get();
 
 
@@ -1296,7 +1296,7 @@ decl_module! {
         /// Base Weight: 75.94 µs
         /// DB Weight:
         /// - Reads: Stash Account, Bonded, Slashing Spans, Locks
-        /// - Writes: Bonded, Ledger, Payee, Validators, Nominators, Stash Account, Locks
+        /// - Writes: Bonded, Ledger, Payee, Validators, guarantors, Stash Account, Locks
         /// # </weight>
         #[weight = T::DbWeight::get().reads_writes(4, 7)
             .saturating_add(76 * WEIGHT_PER_MICROS)]
@@ -1308,7 +1308,7 @@ decl_module! {
 
         /// Pay out all the stakers behind a single validator for a single era.
         ///
-        /// - `validator_stash` is the stash account of the validator. Their nominators, up to
+        /// - `validator_stash` is the stash account of the validator. Their guarantors, up to
         ///   `T::MaxGuarantorRewardedPerValidator`, will also receive their rewards.
         /// - `era` may be any era between `[current_era - history_depth; current_era]`.
         ///
@@ -2307,12 +2307,12 @@ for Module<T> where
             });
 
             if let Some(mut unapplied) = unapplied {
-                let nominators_len = unapplied.others.len() as u64;
+                let guarantors_len = unapplied.others.len() as u64;
                 let reporters_len = details.reporters.len() as u64;
 
                 {
-                    let upper_bound = 1 /* Validator/NominatorSlashInEra */ + 2 /* fetch_spans */;
-                    let rw = upper_bound + nominators_len * upper_bound;
+                    let upper_bound = 1 /* Validator/guarantorSlashInEra */ + 2 /* fetch_spans */;
+                    let rw = upper_bound + guarantors_len * upper_bound;
                     add_db_reads_writes(rw, rw);
                 }
                 unapplied.reporters = details.reporters.clone();
@@ -2323,8 +2323,8 @@ for Module<T> where
                         let slash_cost = (6, 5);
                         let reward_cost = (2, 2);
                         add_db_reads_writes(
-                            (1 + nominators_len) * slash_cost.0 + reward_cost.0 * reporters_len,
-                            (1 + nominators_len) * slash_cost.1 + reward_cost.1 * reporters_len
+                            (1 + guarantors_len) * slash_cost.0 + reward_cost.0 * reporters_len,
+                            (1 + guarantors_len) * slash_cost.1 + reward_cost.1 * reporters_len
                         );
                     }
                 } else {

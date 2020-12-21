611bcdc24cff37fecb7cc1cdee2cb77ee1a13e00
MyronFanQiu <qiufan_glamour@hotmail.com>
2020-12-21 16:58:07 +0800

    bump swork weight 20x and bump spec version

--- cstrml/swork/src/weight.rs
+++ cstrml/swork/src/weight.rs
@@ -8,21 +8,22 @@ use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
 pub struct WeightInfo;
 impl crate::WeightInfo for WeightInfo {
 	fn upgrade() -> Weight {
-		(3_000_000 as Weight)
-			.saturating_add(DbWeight::get().writes(2 as Weight))
+		(97_000_000 as Weight)
+			.saturating_add(DbWeight::get().reads(4 as Weight))
+			.saturating_add(DbWeight::get().writes(4 as Weight))
 	}
 	fn register() -> Weight {
-		(555_000_000 as Weight)
+		(229_659_000_00 as Weight)
 			.saturating_add(DbWeight::get().reads(6 as Weight))
 			.saturating_add(DbWeight::get().writes(4 as Weight))
 	}
 	fn report_works() -> Weight {
-		(495_000_000 as Weight)
+		(139_359_000_00 as Weight)
 			.saturating_add(DbWeight::get().reads(12 as Weight))
 			.saturating_add(DbWeight::get().writes(6 as Weight))
 	}
 	fn chill_pk() -> Weight {
-		(41_000_000 as Weight)
+		(341_000_000 as Weight)
 			.saturating_add(DbWeight::get().reads(6 as Weight))
 			.saturating_add(DbWeight::get().writes(5 as Weight))
 	}

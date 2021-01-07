3abc406dbc6de7fb4c14b2f74c12c39a1993af2c
MyronFanQiu <qiufan_glamour@hotmail.com>
2021-01-07 15:40:37 +0800

    fix market benchmark

--- cstrml/market/src/benchmarking.rs
+++ cstrml/market/src/benchmarking.rs
@@ -3,10 +3,11 @@ use super::*;
 use frame_system::RawOrigin;
 use frame_benchmarking::{benchmarks, account};
 use frame_support::traits::Currency;
+use primitives::*;
 use sp_std::vec;
 
 const SEED: u32 = 0;
-const ACCOUNT_INIT_BALANCE: u32 = 10000000;
+const ACCOUNT_INIT_BALANCE: u32 = 1_000_000_000;
 
 use crate::Module as Market;
 
@@ -17,216 +18,93 @@ fn create_funded_user<T: Config>(string: &'static str, n: u32) -> T::AccountId {
     user
 }
 
-// fn add_market_files<T: Config>(files: Vec<(MerkleRoot, u64, u64)>, user: T::AccountId, pub_key: Vec<u8>) {
-//     for (file, file_size, _) in files.clone().iter() {
-//         let used_info = UsedInfo {
-//             used_size: *file_size,
-//             groups: <BTreeSet<SworkerAnchor>>::new()
-//         };
-//         let mut replicas: Vec<Replica<T::AccountId>> = vec![];
-//         for _ in 0..200 {
-//             let new_replica = Replica {
-//                 who: user.clone(),
-//                 valid_at: 300,
-//                 anchor: pub_key.clone()
-//             };
-//             replicas.push(new_replica);
-//         }
-//         let file_info = FileInfo {
-//             file_size: *file_size,
-//             expired_on: 1000,
-//             claimed_at: 400,
-//             amount: <T as market::Config>::Currency::minimum_balance() * 1000000000u32.into(),
-//             expected_replica_count: 4,
-//             reported_replica_count: 0,
-//             replicas
-//         };
-//         <market::Files<T>>::insert(file, (file_info, used_info));
-//     }
-//     let storage_value = <T as market::Config>::Currency::minimum_balance() * 10000000u32.into();
-//     <T as market::Config>::Currency::make_free_balance_be(&market::Module::<T>::storage_pot(), storage_value);
-// }
+fn build_market_file<T: Config>(user: &T::AccountId, pub_key: &Vec<u8>, file_size: u64, valid_at: BlockNumber, expired_on: BlockNumber, claimed_at: BlockNumber, amount: u32, expected_replica_count: u32)
+    -> (FileInfo<T::AccountId, BalanceOf<T>>, UsedInfo)
+{
+    let used_info = UsedInfo {
+        used_size: file_size,
+        groups: <BTreeSet<SworkerAnchor>>::new()
+    };
+    let mut replicas: Vec<Replica<T::AccountId>> = vec![];
+    for _ in 0..200 {
+        let new_replica = Replica {
+            who: user.clone(),
+            valid_at,
+            anchor: pub_key.clone()
+        };
+        replicas.push(new_replica);
+    }
+    let file_info = FileInfo {
+        file_size,
+        expired_on,
+        claimed_at,
+        amount: T::Currency::minimum_balance() * amount.into(),
+        expected_replica_count,
+        reported_replica_count: 0,
+        replicas
+    };
+    (file_info, used_info)
+}
 
 benchmarks! {
     _{}
 
     register {
         let user = create_funded_user::<T>("user", 100);
-    }: _(RawOrigin::Signed(user), T::Currency::minimum_balance() * 10u32.into())
+    }: _(RawOrigin::Signed(user.clone()), T::Currency::minimum_balance() * 10u32.into())
+    verify {
+        assert_eq!(Market::<T>::merchant_ledgers(&user), MerchantLedger {
+            pledge: T::Currency::minimum_balance() * 10u32.into(),
+            reward: 0u32.into()
+        });
+    }
 
     pledge_extra {
         let user = create_funded_user::<T>("user", 100);
         Market::<T>::register(RawOrigin::Signed(user.clone()).into(), T::Currency::minimum_balance() * 10u32.into()).expect("Something wrong during registering");
-    }: _(RawOrigin::Signed(user), T::Currency::minimum_balance() * 10u32.into())
+    }: _(RawOrigin::Signed(user.clone()), T::Currency::minimum_balance() * 10u32.into())
+    verify {
+        assert_eq!(Market::<T>::merchant_ledgers(&user), MerchantLedger {
+            pledge: T::Currency::minimum_balance() * 20u32.into(),
+            reward: 0u32.into()
+        });
+    }
 
     cut_pledge {
         let user = create_funded_user::<T>("user", 100);
-        Market::<T>::register(RawOrigin::Signed(user.clone()).into(), T::Currency::minimum_balance() * 10u32.into()).expect("Something wrong during registering");
-    }: _(RawOrigin::Signed(user), T::Currency::minimum_balance() * 10u32.into())
+        Market::<T>::register(RawOrigin::Signed(user.clone()).into(), T::Currency::minimum_balance() * 100u32.into()).expect("Something wrong during registering");
+    }: _(RawOrigin::Signed(user.clone()), T::Currency::minimum_balance() * 10u32.into())
+    verify {
+        assert_eq!(Market::<T>::merchant_ledgers(&user), MerchantLedger {
+            pledge: T::Currency::minimum_balance() * 90u32.into(),
+            reward: 0u32.into()
+        });
+    }
 
-    // register {
-    //     let code: Vec<u8> = vec![120,27,83,125,61,206,243,157,236,123,139,206,111,223,205,3,45,141,132,102,64,233,181,89,139,74,159,98,113,136,169,8];
-    //     swork::Module::<T>::upgrade(RawOrigin::Root.into(), code.clone(), EXPIRE_BLOCK_NUMBER.into()).expect("failed to insert code");
-    //     let user: Vec<u8> = vec![166,239,163,116,112,15,134,64,183,119,188,146,199,125,52,68,124,85,136,215,235,124,78,201,132,50,60,125,176,152,48,9];
-    //     let caller = T::AccountId::decode(&mut &user[..]).unwrap_or_default();
-    //     let ias_sig = "VWfhb8pfVTHFcwIfFI9fLQPPvScGKwWOtkhYzlIMP5MT/u81VMAJed37p87YyMNwpqopaTP6/QVLkrZFw6fRgONMY+kRyzzkUDB3gRhRh71ZqZe0R+XHsGi6QH0YnMiXtCnD9oP3vSKx8UqhMKRpn4eCUU2jKLkoUOT8fiwozOnrIfYH5aVLcF65Laomj0trgoFbJlm/Yag7HOA3mQMRgCoBzP+xeKZBCWr/Zh6814mnwb8X79KVpM7suiy+g0KuZQpjH9qE32XsBL7lNizqVji9XiAJwN6pbhDmQaRbB8y46mJ1HkII+SFHCyBWAtdiqH9cTsmbsTjAS/TjoXcphQ==".as_bytes();
-    //     let ias_cert = "MIIEoTCCAwmgAwIBAgIJANEHdl0yo7CWMA0GCSqGSIb3DQEBCwUAMH4xCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJDQTEUMBIGA1UEBwwLU2FudGEgQ2xhcmExGjAYBgNVBAoMEUludGVsIENvcnBvcmF0aW9uMTAwLgYDVQQDDCdJbnRlbCBTR1ggQXR0ZXN0YXRpb24gUmVwb3J0IFNpZ25pbmcgQ0EwHhcNMTYxMTIyMDkzNjU4WhcNMjYxMTIwMDkzNjU4WjB7MQswCQYDVQQGEwJVUzELMAkGA1UECAwCQ0ExFDASBgNVBAcMC1NhbnRhIENsYXJhMRowGAYDVQQKDBFJbnRlbCBDb3Jwb3JhdGlvbjEtMCsGA1UEAwwkSW50ZWwgU0dYIEF0dGVzdGF0aW9uIFJlcG9ydCBTaWduaW5nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqXot4OZuphR8nudFrAFiaGxxkgma/Es/BA+tbeCTUR106AL1ENcWA4FX3K+E9BBL0/7X5rj5nIgX/R/1ubhkKWw9gfqPG3KeAtIdcv/uTO1yXv50vqaPvE1CRChvzdS/ZEBqQ5oVvLTPZ3VEicQjlytKgN9cLnxbwtuvLUK7eyRPfJW/ksddOzP8VBBniolYnRCD2jrMRZ8nBM2ZWYwnXnwYeOAHV+W9tOhAImwRwKF/95yAsVwd21ryHMJBcGH70qLagZ7Ttyt++qO/6+KAXJuKwZqjRlEtSEz8gZQeFfVYgcwSfo96oSMAzVr7V0L6HSDLRnpb6xxmbPdqNol4tQIDAQABo4GkMIGhMB8GA1UdIwQYMBaAFHhDe3amfrzQr35CN+s1fDuHAVE8MA4GA1UdDwEB/wQEAwIGwDAMBgNVHRMBAf8EAjAAMGAGA1UdHwRZMFcwVaBToFGGT2h0dHA6Ly90cnVzdGVkc2VydmljZXMuaW50ZWwuY29tL2NvbnRlbnQvQ1JML1NHWC9BdHRlc3RhdGlvblJlcG9ydFNpZ25pbmdDQS5jcmwwDQYJKoZIhvcNAQELBQADggGBAGcIthtcK9IVRz4rRq+ZKE+7k50/OxUsmW8aavOzKb0iCx07YQ9rzi5nU73tME2yGRLzhSViFs/LpFa9lpQL6JL1aQwmDR74TxYGBAIi5f4I5TJoCCEqRHz91kpG6Uvyn2tLmnIdJbPE4vYvWLrtXXfFBSSPD4Afn7+3/XUggAlc7oCTizOfbbtOFlYA4g5KcYgS1J2ZAeMQqbUdZseZCcaZZZn65tdqee8UXZlDvx0+NdO0LR+5pFy+juM0wWbu59MvzcmTXbjsi7HY6zd53Yq5K244fwFHRQ8eOB0IWB+4PfM7FeAApZvlfqlKOlLcZL2uyVmzRkyR5yW72uo9mehX44CiPJ2fse9Y6eQtcfEhMPkmHXI01sN+KwPbpA39+xOsStjhP9N1Y1a2tQAVo+yVgLgV2Hws73Fc0o3wC78qPEA+v2aRs/Be3ZFDgDyghc/1fgU+7C+P6kbqd4poyb6IW8KCJbxfMJvkordNOgOUUxndPHEi/tb/U7uLjLOgPA==".as_bytes();
-    //     let isv_body = "{\"id\":\"224446224973977124963950294138353548427\",\"timestamp\":\"2020-10-27T07:26:53.412131\",\"version\":3,\"epidPseudonym\":\"4tcrS6EX9pIyhLyxtgpQJuMO1VdAkRDtha/N+u/rRkTsb11AhkuTHsY6UXRPLRJavxG3nsByBdTfyDuBDQTEjMYV6NBXjn3P4UyvG1Ae2+I4lE1n+oiKgLA8CR8pc2nSnSY1Wz1Pw/2l9Q5Er6hM6FdeECgMIVTZzjScYSma6rE=\",\"isvEnclaveQuoteStatus\":\"GROUP_OUT_OF_DATE\",\"platformInfoBlob\":\"1502006504000F00000F0F02040101070000000000000000000B00000B00000002000000000000142ADC0536C0F778E6339B78B7495BDAB064CBC27DA1049CE6739151D0F781995C52276F171A92BE72FDDC4A5602B353742E9DF16256EADC00D3577943656DFEEE1B\",\"isvEnclaveQuoteBody\":\"AgABACoUAAAKAAkAAAAAAP7yPH5zo3mCPOcf8onPvAcAAAAAAAAAAAAAAAAAAAAACBD///8CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABwAAAAAAAAAHAAAAAAAAAHgbU309zvOd7HuLzm/fzQMtjYRmQOm1WYtKn2JxiKkIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACD1xnnferKFHD2uvYqTXdDA8iZ22kCD5xw7h38CMfOngAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADLinsnSTdJyTnaS7pyZvFHa7lg50iRgXVEUDISYg3OPJThwmxiLMuahAQViB3u9UErVI8ip9XlwF+0Es/cjlRk\"}".as_bytes();
-    //     let sig: Vec<u8> = vec![153,15,132,203,16,61,189,174,53,69,117,139,125,120,121,86,243,25,28,226,237,230,56,194,238,228,22,182,116,166,245,27,86,43,129,7,122,13,3,143,247,159,97,239,88,200,8,51,238,45,204,71,25,38,46,164,18,85,82,175,13,48,15,190];
-    // }: {
-    //     swork::Module::<T>::register(RawOrigin::Signed(caller.clone()).into(), ias_sig.to_vec(), ias_cert.to_vec(), caller.clone(), isv_body.to_vec(), sig).expect("Something wrong during registering");
-    // }
-    //
-    // report_works_with_added_files {
-    //     let code: Vec<u8> = vec![120,27,83,125,61,206,243,157,236,123,139,206,111,223,205,3,45,141,132,102,64,233,181,89,139,74,159,98,113,136,169,8];
-    //     swork::Module::<T>::upgrade(RawOrigin::Root.into(), code.clone(), EXPIRE_BLOCK_NUMBER.into()).expect("failed to insert code");
-    //
-    //     // Prepare legal work report
-    //     let user: Vec<u8> = vec![212,53,147,199,21,253,211,28,97,20,26,189,4,169,159,214,130,44,133,88,133,76,205,227,154,86,132,231,165,109,162,125]; // Alice
-    //     let caller = T::AccountId::decode(&mut &user[..]).unwrap_or_default();
-    //     let wr = legal_work_report_with_added_files();
-    //
-    //     // Set block number, system hash and pk in swork
-    //     swork::Module::<T>::insert_pk_info(wr.curr_pk.clone(), code.clone());
-    //     system::Module::<T>::set_block_number(303u32.into());
-    //     let fake_bh:T::Hash = T::Hash::decode(&mut &wr.block_hash[..]).unwrap_or_default();
-    //     let target_block_number:T::BlockNumber = 300u32.into();
-    //     <system::BlockHash<T>>::insert(target_block_number, fake_bh);
-    //
-    //     // Prepare Files in market
-    //     add_market_files::<T>(wr.added_files.clone(), caller.clone(), wr.curr_pk.clone());
-    // }: {
-    //     swork::Module::<T>::report_works(
-    //         RawOrigin::Signed(caller.clone()).into(),
-    //         wr.curr_pk.clone(),
-    //         wr.prev_pk,
-    //         wr.block_number,
-    //         wr.block_hash,
-    //         wr.free,
-    //         wr.used,
-    //         wr.added_files,
-    //         wr.deleted_files,
-    //         wr.srd_root,
-    //         wr.files_root,
-    //         wr.sig
-    //     ).expect("Something wrong during reporting works");
-    // } verify {
-    //     assert_eq!(swork::Module::<T>::free(), wr.free as u128);
-    //     assert_eq!(swork::Module::<T>::used(), wr.used as u128);
-    //     assert_eq!(swork::Module::<T>::reported_in_slot(&wr.curr_pk, wr.block_number), true);
-    // }
-    //
-    // report_works {
-    //     let code: Vec<u8> = vec![120,27,83,125,61,206,243,157,236,123,139,206,111,223,205,3,45,141,132,102,64,233,181,89,139,74,159,98,113,136,169,8];
-    //     swork::Module::<T>::upgrade(RawOrigin::Root.into(), code.clone(), EXPIRE_BLOCK_NUMBER.into()).expect("failed to insert code");
-    //
-    //     // Prepare legal work report
-    //     let user: Vec<u8> = vec![212,53,147,199,21,253,211,28,97,20,26,189,4,169,159,214,130,44,133,88,133,76,205,227,154,86,132,231,165,109,162,125]; // Alice
-    //     let caller = T::AccountId::decode(&mut &user[..]).unwrap_or_default();
-    //     let wr = legal_work_report_with_added_files();
-    //
-    //     // Set block number, system hash and pk in swork at 300
-    //     swork::Module::<T>::insert_pk_info(wr.curr_pk.clone(), code.clone());
-    //     system::Module::<T>::set_block_number(303u32.into());
-    //     let fake_bh:T::Hash = T::Hash::decode(&mut &wr.block_hash[..]).unwrap_or_default();
-    //     let target_block_number:T::BlockNumber = 300u32.into();
-    //     <system::BlockHash<T>>::insert(target_block_number, fake_bh);
-    //
-    //     // Prepare Files in market
-    //     add_market_files::<T>(wr.added_files.clone(), caller.clone(), wr.curr_pk.clone());
-    //
-    //     // Report works at 300
-    //     swork::Module::<T>::report_works(
-    //         RawOrigin::Signed(caller.clone()).into(),
-    //         wr.curr_pk.clone(),
-    //         wr.prev_pk,
-    //         wr.block_number,
-    //         wr.block_hash,
-    //         wr.free,
-    //         wr.used,
-    //         wr.added_files,
-    //         wr.deleted_files,
-    //         wr.srd_root,
-    //         wr.files_root,
-    //         wr.sig
-    //     ).expect("Something wrong during reporting works");
-    //
-    //     let wr = legal_work_report_with_deleted_files();
-    //     // Set block number, system hash and pk in swork at 600
-    //     system::Module::<T>::set_block_number(603u32.into());
-    //     let fake_bh:T::Hash = T::Hash::decode(&mut &wr.block_hash[..]).unwrap_or_default();
-    //     let target_block_number:T::BlockNumber = 600u32.into();
-    //     <system::BlockHash<T>>::insert(target_block_number, fake_bh);
-    // }: {
-    //     swork::Module::<T>::report_works(
-    //         RawOrigin::Signed(caller.clone()).into(),
-    //         wr.curr_pk.clone(),
-    //         wr.prev_pk,
-    //         wr.block_number,
-    //         wr.block_hash,
-    //         wr.free,
-    //         wr.used,
-    //         wr.added_files,
-    //         wr.deleted_files,
-    //         wr.srd_root,
-    //         wr.files_root,
-    //         wr.sig
-    //     ).expect("Something wrong during reporting works");
-    // } verify {
-    //     assert_eq!(swork::Module::<T>::free(), wr.free as u128);
-    //     assert_eq!(swork::Module::<T>::used(), wr.used as u128);
-    //     assert_eq!(swork::Module::<T>::reported_in_slot(&wr.curr_pk, wr.block_number), true);
-    // }
-    //
-    // create_group {
-    //     let owner: T::AccountId = account("owner", 0, SEED);
-    // }: {
-    //     swork::Module::<T>::create_group(RawOrigin::Signed(owner.clone()).into()).expect("Something wrong during creating group");
-    // } verify {
-    //     assert_eq!(<swork::Groups<T>>::contains_key(&owner), true);
-    // }
-    //
-    // join_group {
-    //     let owner: T::AccountId = account("owner", 0, SEED);
-    //     let member: T::AccountId = account("member", 0, SEED);
-    //     swork::Module::<T>::create_group(RawOrigin::Signed(owner.clone()).into()).expect("Something wrong during creating group");
-    //
-    //     let code: Vec<u8> = vec![120,27,83,125,61,206,243,157,236,123,139,206,111,223,205,3,45,141,132,102,64,233,181,89,139,74,159,98,113,136,169,8];
-    //     swork::Module::<T>::upgrade(RawOrigin::Root.into(), code.clone(), EXPIRE_BLOCK_NUMBER.into()).expect("failed to insert code");
-    //
-    //     // Prepare legal work report
-    //     let user: Vec<u8> = vec![212,53,147,199,21,253,211,28,97,20,26,189,4,169,159,214,130,44,133,88,133,76,205,227,154,86,132,231,165,109,162,125]; // Alice
-    //     let caller = T::AccountId::decode(&mut &user[..]).unwrap_or_default();
-    //     let wr = legal_work_report_with_deleted_files();
-    //
-    //     swork::Module::<T>::insert_pk_info(wr.curr_pk.clone(), code.clone());
-    //
-    //     // Set block number, system hash and pk in swork at 600
-    //     system::Module::<T>::set_block_number(603u32.into());
-    //     let fake_bh:T::Hash = T::Hash::decode(&mut &wr.block_hash[..]).unwrap_or_default();
-    //     let target_block_number:T::BlockNumber = 600u32.into();
-    //     <system::BlockHash<T>>::insert(target_block_number, fake_bh);
-    //
-    //     swork::Module::<T>::report_works(
-    //         RawOrigin::Signed(member.clone()).into(),
-    //         wr.curr_pk.clone(),
-    //         wr.prev_pk,
-    //         wr.block_number,
-    //         wr.block_hash,
-    //         wr.free,
-    //         wr.used,
-    //         wr.added_files,
-    //         wr.deleted_files,
-    //         wr.srd_root,
-    //         wr.files_root,
-    //         wr.sig
-    //     ).expect("Something wrong during reporting works");
-    // }: {
-    //     swork::Module::<T>::join_group(RawOrigin::Signed(member.clone()).into(), T::Lookup::unlookup(owner.clone())).expect("Something wrong during joining group");
-    // } verify {
-    //     assert_eq!(<swork::Groups<T>>::contains_key(&owner), true);
-    //     assert_eq!(swork::Module::<T>::groups(&owner), BTreeSet::from_iter(vec![member.clone()].into_iter()))
-    // }
+    place_storage_order {
+        let user = create_funded_user::<T>("user", 100);
+        let cid = vec![0];
+        let file_size: u64 = 10;
+        let pub_key = vec![1];
+        <self::Files<T>>::insert(&cid, build_market_file::<T>(&user, &pub_key, file_size, 300, 1000, 400, 1000, 4));
+        system::Module::<T>::set_block_number(600u32.into());
+    }: _(RawOrigin::Signed(user.clone()), cid.clone(), file_size, T::Currency::minimum_balance() * 10u32.into(), true)
+    verify {
+        assert_eq!(Market::<T>::files(&cid).unwrap_or_default().0.claimed_at, 600);
+    }
+
+    calculate_reward {
+        let user = create_funded_user::<T>("user", 100);
+        let cid = vec![0];
+        let file_size: u64 = 10;
+        let pub_key = vec![1];
+        <self::Files<T>>::insert(&cid, build_market_file::<T>(&user, &pub_key, file_size, 300, 1000, 400, 1000, 4));
+        system::Module::<T>::set_block_number(600u32.into());
+    }: _(RawOrigin::Signed(user.clone()), cid.clone())
+    verify {
+        assert_eq!(Market::<T>::files(&cid).unwrap_or_default().0.claimed_at, 600);
+    }
+
 }
 
 #[cfg(test)]
@@ -256,33 +134,20 @@ mod tests {
         });
     }
 
-    // #[test]
-    // fn report_works() {
-    //     ExtBuilder::default().build().execute_with(|| {
-    //         assert_ok!(test_benchmark_report_works::<Test>());
-    //     });
-    // }
-    //
-    // #[test]
-    // fn report_works_with_added_files() {
-    //     ExtBuilder::default().build().execute_with(|| {
-    //         assert_ok!(test_benchmark_report_works_with_added_files::<Test>());
-    //     });
-    // }
-    //
-    // #[test]
-    // fn create_group() {
-    //     ExtBuilder::default().build().execute_with(|| {
-    //         assert_ok!(test_benchmark_create_group::<Test>());
-    //     });
-    // }
-    //
-    // #[test]
-    // fn join_group() {
-    //     ExtBuilder::default().build().execute_with(|| {
-    //         assert_ok!(test_benchmark_join_group::<Test>());
-    //     });
-    // }
+    #[test]
+    fn place_storage_order() {
+        new_test_ext().execute_with(|| {
+            assert_ok!(test_benchmark_place_storage_order::<Test>());
+        });
+    }
+
+    #[test]
+    fn calculate_reward() {
+        new_test_ext().execute_with(|| {
+            assert_ok!(test_benchmark_calculate_reward::<Test>());
+        });
+    }
+
 }
 
 

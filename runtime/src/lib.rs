6e396e0b5bacdd6138fca70c2c60583521cab2b8
Zikun Fan <zikunvan@gmail.com>
2021-01-26 14:31:20 +0800

    Remove runtime upgrade

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -102,7 +102,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("crust"),
     impl_name: create_runtime_str!("crustio-crust"),
     authoring_version: 1,
-    spec_version: 14,
+    spec_version: 1,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1

2b879957f8d23a33d1666e64a72e9f296fe86662
MyronFanQiu <qiufan_glamour@hotmail.com>
2021-02-23 14:54:43 +0800

    latest balance module

--- cstrml/balances/src/benchmarking.rs
+++ cstrml/balances/src/benchmarking.rs
@@ -1,5 +1,19 @@
-// Copyright (C) 2019-2021 Crust Network Technologies Ltd.
-// This file is part of Crust.
+// This file is part of Substrate.
+
+// Copyright (C) 2020-2021 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 //! Balances pallet benchmarking.
 
@@ -8,7 +22,7 @@
 use super::*;
 
 use frame_system::RawOrigin;
-use frame_benchmarking::{benchmarks, account, whitelisted_caller};
+use frame_benchmarking::{benchmarks, account, whitelisted_caller, impl_benchmark_test_suite};
 use sp_runtime::traits::Bounded;
 
 use crate::Module as Balances;
@@ -19,8 +33,6 @@ const ED_MULTIPLIER: u32 = 10;
 
 
 benchmarks! {
-	_ { }
-
 	// Benchmark `transfer` extrinsic with the worst possible conditions:
 	// * Transfer will kill the sender account.
 	// * Transfer will create the recipient account.
@@ -132,51 +144,8 @@ benchmarks! {
 	}
 }
 
-#[cfg(test)]
-mod tests {
-	use super::*;
-	use crate::tests_composite::{ExtBuilder, Test};
-	use frame_support::assert_ok;
-
-	#[test]
-	fn transfer() {
-		ExtBuilder::default().build().execute_with(|| {
-			assert_ok!(test_benchmark_transfer::<Test>());
-		});
-	}
-
-	#[test]
-	fn transfer_best_case() {
-		ExtBuilder::default().build().execute_with(|| {
-			assert_ok!(test_benchmark_transfer_best_case::<Test>());
-		});
-	}
-
-	#[test]
-	fn transfer_keep_alive() {
-		ExtBuilder::default().build().execute_with(|| {
-			assert_ok!(test_benchmark_transfer_keep_alive::<Test>());
-		});
-	}
-
-	#[test]
-	fn transfer_set_balance_creating() {
-		ExtBuilder::default().build().execute_with(|| {
-			assert_ok!(test_benchmark_set_balance_creating::<Test>());
-		});
-	}
-
-	#[test]
-	fn transfer_set_balance_killing() {
-		ExtBuilder::default().build().execute_with(|| {
-			assert_ok!(test_benchmark_set_balance_killing::<Test>());
-		});
-	}
-
-	#[test]
-	fn force_transfer() {
-		ExtBuilder::default().build().execute_with(|| {
-			assert_ok!(test_benchmark_force_transfer::<Test>());
-		});
-	}
-}
+impl_benchmark_test_suite!(
+	Balances,
+	crate::tests_composite::ExtBuilder::default().build(),
+	crate::tests_composite::Test,
+);

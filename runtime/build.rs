b2bdabde7d1283dcb262aceac69ccec0f161aebe
MyronFanQiu <49134743+MyronFanQiu@users.noreply.github.com>
2021-03-18 19:42:51 +0800

    Remove useless GNU clarify (#541)

--- runtime/build.rs
+++ runtime/build.rs
@@ -1,9 +1,6 @@
 // Copyright (C) 2019-2021 Crust Network Technologies Ltd.
 // This file is part of Crust.
 
-// You should have received a copy of the GNU General Public License
-// along with Substrate. If not, see <http://www.gnu.org/licenses/>.
-
 use substrate_wasm_builder::WasmBuilder;
 
 fn main() {

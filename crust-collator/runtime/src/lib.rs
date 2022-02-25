7e7db2b29c6b43548608581853968fd5c5d7f705
MyronFanQiu <qiufan_glamour@hotmail.com>
2022-02-25 10:49:35 +0800

    bump version to 8

--- crust-collator/runtime/src/lib.rs
+++ crust-collator/runtime/src/lib.rs
@@ -117,7 +117,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("crust-collator"),
 	impl_name: create_runtime_str!("crust-collator"),
 	authoring_version: 1,
-	spec_version: 10,
+	spec_version: 8,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

ab9ca8cbf06eb332cfd8ab3eb280fc97fd6efc73
Myron <49134743+mmyyrroonn@users.noreply.github.com>
2022-09-19 10:30:18 +0800

    bump spec version to 14 (#902)

--- crust-collator/runtime/src/lib.rs
+++ crust-collator/runtime/src/lib.rs
@@ -127,7 +127,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("crust-collator"),
 	impl_name: create_runtime_str!("crust-collator"),
 	authoring_version: 1,
-	spec_version: 13,
+	spec_version: 14,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

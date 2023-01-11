b7d84d854d15f2283accdc96fdd6f23c49ab075e
MyronFanQiu <qiufan_glamour@hotmail.com>
2023-01-11 14:42:25 +0800

    bump version to 3

--- crust-collator/runtime/src/lib.rs
+++ crust-collator/runtime/src/lib.rs
@@ -130,7 +130,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot-crust-parachain"),
 	impl_name: create_runtime_str!("polkadot-crust-parachain"),
 	authoring_version: 1,
-	spec_version: 2,
+	spec_version: 3,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

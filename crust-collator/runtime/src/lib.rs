14b52874e909f34e1a2729dc595ad1e98db525de
BrianWu <wuhaixian1984@163.com>
2024-03-11 15:11:21 +0800

    Update spec version from 17 to 18 to reflect the latest changes in chain spec and runtime.

--- crust-collator/runtime/src/lib.rs
+++ crust-collator/runtime/src/lib.rs
@@ -131,7 +131,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("crust-collator"),
 	impl_name: create_runtime_str!("crust-collator"),
 	authoring_version: 1,
-	spec_version: 17,
+	spec_version: 18,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

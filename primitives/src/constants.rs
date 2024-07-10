f3d28b1559bfc8d8064867e3b841775adcd4a2b5
BrianWu <wuhaixian1984@163.com>
2024-07-10 12:52:40 +0800

    Fix failed unit tests in staking pallet

--- primitives/src/constants.rs
+++ primitives/src/constants.rs
@@ -70,6 +70,9 @@ pub mod swork {
     #[cfg(not(feature = "test"))]
     pub const REPORT_SLOT: u64 = EPOCH_DURATION_IN_BLOCKS as u64;
 
+    #[cfg(feature = "test")]
+    pub const UPDATE_OFFSET: u32 = (REPORT_SLOT / 3) as u32;
+    #[cfg(not(feature = "test"))]
     pub const UPDATE_OFFSET: u32 = (REPORT_SLOT / 6) as u32;
     pub const END_OFFSET: u32 = 1;
 }
